7d1786738f8bd104cf0ccca390a3371bac945820
Tom Huntington <55266932+tom-huntington@users.noreply.github.com>
2022-06-20 19:28:43 +1200

    removed commented code

--- git_gutter.kak
+++ git_gutter.kak
@@ -1,21 +1,14 @@
-declare-option line-specs git_diff_line_specs
-declare-option str gitgutter_buffer
+declare-option -hidden line-specs git_diff_line_specs
+declare-option -hidden str gitgutter_buffer
 
 declare-option -hidden -docstring \
 "Path to git_gutter.sh script." \
 str git_gutter_sh_source %sh{ echo "${kak_source%%.kak}.sh" }
 
 hook global ModeChange pop:insert:.* gitdiff
+hook global BufCreate .* gitdiff
 
-# # echo %sh{ "${kak_source%%.kak}.sh" }
-# hook global ModeChange pop:insert:.* %{ echo \ # %{
-#     # set-option buffer temp_filename %sh{ mktemp -t git_gutter.XXXXXX }
-#     # write! %opt{temp_filename}
-#     # evaluate-commands %sh{ . "${kak_opt_git_gutter_sh_source}" } }
-#     %sh{ . "${kak_opt_git_gutter_sh_source}" } 
-# }
-
-define-command gitdiff %{
+define-command -hidden gitdiff %{
     set-option buffer gitgutter_buffer %sh{ mktemp -t gitgutter.buffer.XXXXXX }
     write! -force %opt{gitgutter_buffer}
     evaluate-commands %sh{
--- git_gutter.sh
+++ git_gutter.sh
@@ -42,7 +42,6 @@ process_added()
     to_line=$2
     to_count=$3
     index=0
-    # echo "process_added: from_count=${from_count} to_line=${to_line} to_count=${to_count}"
 
     while [ $index -lt $to_count ]; do
         line_number=$((to_line+index))
@@ -54,11 +53,8 @@ process_added()
 
 process_removed()
 {
-    # from_line=$1
     to_line=$1
     index=0
-    # echo "process_removed: to_line=${to_line}"
-    # return 0
 
     [ $to_line -eq 0 ] \
         && return 0
@@ -72,7 +68,6 @@ process_modified()
     to_line=$1
     to_count=$2
     index=0
-    # echo "process_added: from_count=${from_count} to_line=${to_line} to_count=${to_count}"
 
     while [ $index -lt $to_count ]; do
         line_number=$((to_line+index))
@@ -89,7 +84,6 @@ process_modified_and_added()
     to_line=$2
     to_count=$3
     index=0
-    # echo "process_modified_and_added: from_count=${from_count} to_line=${to_line} to_count=${to_count}"
 
     while [ $index -lt $from_count ]; do
         line_number=$((to_line+index))
@@ -110,7 +104,6 @@ process_modified_and_removed()
     to_line=$1
     to_count=$2
     index=0
-    # echo "process_modified_and_removed: to_line=${to_line} to_count=${to_count}"
 
     while [ $index -lt $to_count ]; do
         line_number=$((to_line+index))
@@ -126,59 +119,40 @@ process_hunk()
     from_count=$2
     to_line=$3
     to_count=$4
-    # echo "process_hunk from_line=${from_line} from_count=${from_count} to_count=${to_count} to_line=${to_line}"
-    # echo "processed_hunk -------------"
     
     is_added $from_count $to_count \
         && process_added $from_count $to_line $to_count \
         && return 0
-    # echo "passed is_added"
     is_removed $from_count $to_count \
         && process_removed $to_line \
         && return 0
-    # echo "passed is_removed"
     is_modified $from_count $to_count \
         && process_modified $to_line $to_count \
         && return 0
     is_modified_and_added $from_count $to_count \
         && process_modified_and_added $from_count $to_line $to_count \
         && return 0
-    # echo "passed is_modified_and_added"
     is_modified_and_removed $from_count $to_count \
         && process_modified_and_removed $to_line $to_count \
         && return 0
-    # echo "not is_modified_and_removed"
-
 }
 
 parse_diff_line()
 {
-    # echo "${2} $(echo $3 | sed 's/,/ /g')"
-    # echo "10 10 2"
     echo "$1" | sed -rn 's/^@@ -([0-9]+),?([0-9]*) \+([0-9]+),?([0-9]*) @@.*/\1 \2 \3 \4 /p' | sed 's/  / 1 /g'
 }
 
 diff_temp_files()
 {
     gitout="$(git --no-pager diff --no-ext-diff --no-color -U0 ${temp_head} ${temp_buffer} | grep '^@@ ')"
-    # modified_lines=""
-    # echo "$gitout"
-    # echo "echo temp_head=${temp_head} end filename=${temp_buffer} end" # set-option buffer git_diff_line_specs %val{timestamp} ${modified_lines}"
-    cat "$gitout" > gitdiffout.txt
-    
 
     echo "${gitout}" | {
         while IFS='' read -r LINE <&0 || [ -n "${LINE}" ]; do
-            # echo "line: ${LINE}
             diff_array="$(parse_diff_line "${LINE}")"
-            # echo "from_line from_count to_line to_count"
-            # echo "$diff_array"
             additional_modified_lines=$(process_hunk $diff_array)
             modified_lines="${modified_lines}${additional_modified_lines}"
         done
-        # echo "$gitout"
         echo "set-option buffer git_diff_line_specs %val{timestamp} ${modified_lines}"
-        # echo "isas echo buffile=${kak_buffile} end filename=${kak_opt_gitgutter_temp_buffer} end" # set-option buffer git_diff_line_specs %val{timestamp} ${modified_lines}"
     }
 }
 
@@ -186,14 +160,15 @@ git_diff()
 {
     temp_buffer="$1"
     buffile="$2"
-    temp_head=$(mktemp -t gitgutter.head.XXXXXX)
-    repo_relative_file="HEAD:$(git ls-files --full-name $buffile)"
-    git --no-pager show $repo_relative_file > "$temp_head" \
-        && diff_temp_files
 
-    cat "$temp_head" > gitshow.txt
-    cat "$temp_buffer" > buffer.txt
+    [ -e "${buffile}" ] \
+        && cd "$(dirname "${buffile}")" \
+        && temp_head=$(mktemp -t gitgutter.head.XXXXXX) \
+        && repo_relative_file="$(git ls-files --full-name $buffile)" \
+        && git --no-pager show "HEAD:$repo_relative_file" > "$temp_head" \
+        && diff_temp_files \
+        || echo "set-option buffer git_diff_line_specs %val{timestamp}"
 
-    rm -f "$temp_head" "$temp_buffer"
+    rm "$temp_head" "$temp_buffer"
 }
 
